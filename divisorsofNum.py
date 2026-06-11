ee9a2f9f40efd21bd713970528ce20ef5a740b8b
Kavya Hosamane Jayanna <125850274+kavyahj04@users.noreply.github.com>
2026-06-11 09:38:37 -0400

    added divisors  code

--- divisorsofNum.py
+++ divisorsofNum.py
@@ -1,10 +1,16 @@
-def divisors(n):
-    nums = []
-    for i in range(n):
-        if i > 0 and n % i == 0:
-            nums.append(i)
-    print(nums)
-    return nums
+# def divisors(n):
+#     nums = []
+#     for i in range(n):
+#         if i > 0 and n % i == 0:
+#             nums.append(i)
+#     print(nums)
+#     return nums
 
-x = int(input())
-divisors(x)
+import math
+
+def divisors(n):
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0 and  i != n // i: 
+            print(i, int(n/i))
+
+divisors(8)
