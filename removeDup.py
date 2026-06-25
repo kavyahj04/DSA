062e1489ac0ea3de89765a6fd32e60d97b44b590
kavyahj04 <khj@binghamton.edu>
2026-06-25 11:30:42 -0400

    pushed remove duplicates

--- removeDup.py
+++ removeDup.py
@@ -7,3 +7,14 @@ def removeDupFromSorted(arr):
 
 removeDupFromSorted([1,1,2,2,3,3,4,4])
         
+def removeDupFromPointers(arr):
+    k = 0
+
+    for i in range(len(arr)):
+        if arr[i] != arr[k]:
+            k += 1
+            arr[k] = arr[i]
+    print(arr)
+    return arr
+
+removeDupFromPointers([1,1,2,2,3,3,4,4])
