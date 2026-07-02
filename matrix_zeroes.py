f9e8957429730e2c09724dd27cae43c58cb669b4
kavyahj04 <khj@binghamton.edu>
2026-07-02 11:36:30 -0400

    matrix_zeroes_Optimal

--- matrix_zeroes.py
+++ matrix_zeroes.py
@@ -62,3 +62,39 @@ makeZeroes2(matrix)
 # Time - O(2 * n * m)
 
 # Space - O(n) + O(m)
+
+
+# Optimal 
+matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+
+def makeZeroes3(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    col0 = 1
+
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                if j != 0:
+                    matrix[0][j] = 0
+                else:
+                    col0 = 0
+    for i in range(1, rows):
+        for j in range(1, cols):
+            if matrix[i][j] != 0:
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
+    
+    if matrix[0][0] == 0:
+        for j in range(cols):
+            matrix[0][j] = 0
+    
+    if col0 == 0:
+        for i in range(rows):
+            matrix[i][0] = 0
+    print(matrix)
+    return matrix
+
+makeZeroes3(matrix)
