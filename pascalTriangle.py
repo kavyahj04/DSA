0939cb5fb08166e8aced4c8ef71d3c4c56d344fd
kavyahj04 <khj@binghamton.edu>
2026-07-04 09:58:43 -0400

    pascal triangle get all row elements

--- pascalTriangle.py
+++ pascalTriangle.py
@@ -4,14 +4,35 @@ def pascalTriangle(row, col):
     for i in range(col):
         print(f"row {row}")
         print(f"col {col}")
-        print(f"{res}* ({row} - {i})")
+        # print(f"{res}* ({row} - {i})")
         res = res * (row - i)
-        print(f"{res} // ({i} + 1)")
+        # print(f"{res} // ({i} + 1)")
         res = res // (i + 1)
         
-    print(res)
+    print(f"element - {res}")
     return res
 
-# pass row -1, col -1
-row, col = 5, 1
-pascalTriangle(row-1, col-1)
+# # pass row -1, col -1
+# row, col = 5, 1
+# pascalTriangle(row-1, col-1)
+
+
+# Print any given row
+
+def pascal2(row):
+    for i in range(row):
+        print(pascalTriangle(row-1,i))
+
+# pascal2(5)
+
+def pascal2Optimal(row):
+    ans = 1
+    print(ans)
+    for i in range(1,row):
+        ans = ans * (row - i)
+        ans = ans // i
+        print(ans)
+    # print(1)
+
+
+pascal2Optimal(5)
