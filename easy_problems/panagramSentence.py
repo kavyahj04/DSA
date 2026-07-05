d1fda51e9fc5ae1ee04b02b4f5c92d29aa5b1be0
kavyahj04 <khj@binghamton.edu>
2026-07-05 10:11:14 -0400

    leetcode easy problems

---
+++ easy_problems/panagramSentence.py
@@ -0,0 +1,7 @@
+ def checkIfPangram(self, sentence: str) -> bool:
+        seen_letters = set(sentence)
+        for i in range(ord('a'), ord('z') + 1):
+            if chr(i) not in seen_letters:
+                return False  
+                
+        return True
