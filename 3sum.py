74534c03672ede8d860ef7c47206afa3443f8689
kavyahj04 <khj@binghamton.edu>
2026-07-07 12:13:16 -0400

    3 sum optimal

--- 3sum.py
+++ 3sum.py
@@ -42,11 +42,12 @@ def sum3_2(nums):
 
 def sum3_3(nums):
     ls = []
+    nums = sorted(nums)
     for i in range(len(nums)):
-        if i > 0  and num[i] != num[i-1]:
+        if i > 0  and nums[i] == nums[i-1]:
             continue
         j = i + 1
-        k = len(arr) - 1
+        k = len(nums) - 1
         while j < k:
             sum_ = nums[i] + nums[j] + nums[k]
             if sum_ > 0:
@@ -56,7 +57,9 @@ def sum3_3(nums):
             else:
                 temp = [nums[i], nums[j], nums[k]]
                 ls.append(temp)
-                while j < k and nums[j] == nums[j -1]:
+                j += 1
+                k -= 1
+                while j < k and nums[j] == nums[j - 1]:
                     j += 1
                 while j < k and nums[k] == nums[k + 1]:
                     k -= 1
