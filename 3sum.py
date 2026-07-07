d9bccea8e53ff67dee4afd3659adcb9f33c47f34
kavyahj04 <khj@binghamton.edu>
2026-07-07 12:03:12 -0400

    3 sum better

--- 3sum.py
+++ 3sum.py
@@ -13,4 +13,54 @@ def sum3(nums):
     return [list(t) for t in st]
 
 nums = [-1,0,1,2,-1,-4]
-sum3(nums)
+# sum3(nums)
+
+# Time and Space complexity
+# Time complexity: O(n³)
+# Space complexity: O(n³) (worst case, for storing unique triplets in st) — auxiliary space excluding output is O(1) per iteration.
+
+
+# Better Approach 
+
+def sum3_2(nums):
+    st = set()
+    for i in range(len(nums)):
+        hashSt = set()
+        for j in range(i+1, len(nums)):
+            third = -(nums[i] + nums[j])
+            if third in hashSt:
+                st.add(tuple(sorted([nums[i], nums[j],third])))
+            hashSt.add(nums[j])
+        
+    print([list(t) for t in st])
+    return [list(t) for t in st]
+
+# sum3_2(nums)
+
+
+# Optimal Approach 
+
+def sum3_3(nums):
+    ls = []
+    for i in range(len(nums)):
+        if i > 0  and num[i] != num[i-1]:
+            continue
+        j = i + 1
+        k = len(arr) - 1
+        while j < k:
+            sum_ = nums[i] + nums[j] + nums[k]
+            if sum_ > 0:
+                k -= 1
+            elif sum_ < 0:
+                j += 1
+            else:
+                temp = [nums[i], nums[j], nums[k]]
+                ls.append(temp)
+                while j < k and nums[j] == nums[j -1]:
+                    j += 1
+                while j < k and nums[k] == nums[k + 1]:
+                    k -= 1
+    print(ls)
+    return ls
+
+sum3_3(nums)
