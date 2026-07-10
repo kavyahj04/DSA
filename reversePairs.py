1ce31ab915d385e6a26984b8dde12387664f7088
kavyahj04 <khj@binghamton.edu>
2026-07-10 10:15:55 -0400

    reverse pairs

---
+++ reversePairs.py
@@ -0,0 +1,49 @@
+
+def merge(arr, low, mid, high):
+    temp = []
+    left = low
+    right = mid+1
+
+    while left <= mid and right <= high:
+        if arr[left] <= arr[right]:
+            temp.append(arr[left])
+            left += 1
+        else:
+            temp.append(arr[right])
+            right += 1
+    
+    while left <= mid:
+        temp.append(arr[left])
+        left += 1
+    
+    while right <= high:
+        temp.append(arr[right])
+        right += 1
+    
+    for i in range(low, high+1):
+        arr[i] = temp[i - low]
+
+def countPairs(arr, low, mid, high):
+    right = mid + 1
+    cnt = 0
+    for i in range(low, mid + 1):
+        while (right <= high and arr[i] > 2 * arr[right]):
+            right += 1
+        cnt += (right - (mid + 1))
+    return cnt
+
+
+def reversePairs(arr, low, high):
+    cnt = 0
+    if low >= high:
+        return 0
+        
+    mid = (low + high) // 2
+    cnt += reversePairs(arr, low, mid)
+    cnt += reversePairs(arr, mid+1, high)
+    cnt += countPairs(arr, low, mid, high)
+    merge(arr, low, mid, high)
+    print(cnt)
+    return cnt
+
+reversePairs([1,3,2,3,1], 0, 4)
