f2c2929eac36a5d3bff19e77b6863273a614648f
kavyahj04 <khj@binghamton.edu>
2026-07-16 09:57:04 -0400

    search in rotated sorted array

---
+++ 04_Binary_Search/06_search_in_rotatedSortedArr.py
@@ -0,0 +1,31 @@
+class SeachElement:
+    def __init__(self, nums, target):
+        self.nums = nums
+        self.target = target
+
+    def searchInSorted(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target == nums[m]:
+                print(f"The search Index position is - {m}")
+                return m
+            if nums[m] >= nums[l]:
+                if target >= nums[l] and target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if target > nums[m] and target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+        print(f"The search Index position is - {m}")
+        return m
+
+nums = [4,5,6,7,0,1,2]
+target = 6
+search = SeachElement(nums, target)
+search.searchInSorted(search.nums, search.target)
+
+
