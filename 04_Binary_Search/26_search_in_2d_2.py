b3dfb2525a30a9e1787f013347dae744a90ebf8f
kavyahj04 <khj@binghamton.edu>
2026-07-29 17:56:13 -0400

    updated few changes

--- 04_Binary_Search/26_search_in_2d_2.py
+++ 04_Binary_Search/26_search_in_2d_2.py
@@ -32,6 +32,6 @@ def search2(matrix, target):
 
 matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
 target = 6
-search1(matrix, target)
+search2(matrix, target)
 
 
