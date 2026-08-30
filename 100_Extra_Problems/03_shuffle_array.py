e5e459868ead70cd40da891b220c67606c000b22
kavyahj04 <khj@binghamton.edu>
2026-08-29 20:46:43 -0400

    extra problem

--- 100_Extra_Problems/03_shuffle_array.py
+++ 100_Extra_Problems/03_shuffle_array.py
@@ -13,9 +13,3 @@ class Solution:
         random.shuffle(self.nums)
         return self.nums
         
-
-
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(nums)
-# param_1 = obj.reset()
-# param_2 = obj.shuffle()
