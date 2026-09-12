203ef11fcf7068c9ea42f113998e56e1e346314c
kavyahj04 <khj@binghamton.edu>
2026-09-12 09:09:48 -0400

    delete middle node

---
+++ 07_Linked_List/17_delete_mid_node.py
@@ -0,0 +1,9 @@
+def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        slow = dummy
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        slow.next = slow.next.next
+        return dummy.next
