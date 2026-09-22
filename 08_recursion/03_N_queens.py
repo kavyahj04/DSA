33b876d124f302ad41c60a81fb4ddc43139c10bc
kavyahj04 <khj@binghamton.edu>
2026-09-22 09:06:54 -0400

    n queens problem

---
+++ 08_recursion/03_N_queens.py
@@ -0,0 +1,36 @@
+class Solution:
+    def solveNQueens(self, n: int) -> list[list[str]]:
+        board = [-1] * n
+        all_sol = []
+        result = []
+        def is_safe(queen, col):
+            for prev_queen in range(queen):
+                prev_col = board[prev_queen]
+                if prev_col == col:
+                    return False
+                
+                if abs(prev_col-col) == abs(prev_queen-queen):
+                    return False
+            return True
+        
+        def backtrack(queen):
+            if queen == n:
+                formatted_board = []
+                for col in board:
+                    row_str = "." * col + "Q" + "." * (n - col - 1)
+                    formatted_board.append(row_str)
+                all_sol.append(formatted_board)
+                return
+            
+            for col in range(n):
+                if is_safe(queen, col):
+                    board[queen] = col
+                    backtrack(queen+1)
+                    board[queen] = -1
+                else:
+                    continue
+            return all_sol
+        backtrack(0)
+        return all_sol
+
+            
